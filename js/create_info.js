31ff63e99430d85c9861808258d5ca812449be52
Karakolito <60663821+Karakolito@users.noreply.github.com>
2023-06-29 19:58:44 -0300

    desb2

--- js/create_info.js
+++ js/create_info.js
@@ -7,7 +7,7 @@ const container_sillas = document.querySelector(".container_info")
 
 container_sillas.innerHTML = `<div class="column">
     <h2>${searchedSilla.name}</h2>
-    <img src="${searchedSilla.image}" alt="Silla imagen">
+    <img src="${searchedSilla.img}" alt="Silla imagen">
 </div>
 <div class="column">
     <h2>Detalles:</h2>
